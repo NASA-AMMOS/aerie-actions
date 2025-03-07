b30da7851df8fc7580a21e99f350c406c20ee926
Dan Delany <daniel.t.delany@jpl.nasa.gov>
2025-03-07 09:36:31 -0800

    queries for reading and writing sequences from the helper library

--- src/helpers.ts
+++ src/helpers.ts
@@ -11,16 +11,40 @@ export class Actions {
 
   async listSequences(): Promise<any> {
     console.warn(`List files - Not yet implemented`);
-    return await this.pool.query(`
-      SELECT name, id FROM user_sequence WHERE workspace_id = $1;
+    const result = await this.pool.query(`
+      select name, id, created_at, owner, parcel_id, updated_at, workspace_id 
+        from sequencing.user_sequence
+        where workspace_id = $1;
     `, [this.workspaceId]);
+    const rows = result.rows;
+    return rows;
   }
-  async readSequence(path: string): Promise<string> {
-    console.warn(`Read file ${path} - Not yet implemented`);
-    return "not implemented";
+  async readSequence(name: string): Promise<any> {
+    const result = await this.pool.query(`
+      select definition, seq_json, name, id, created_at, owner, parcel_id, updated_at, workspace_id
+      from sequencing.user_sequence
+        where name = $1 
+          and workspace_id = $2;
+    `, [name, this.workspaceId]);
+    const rows = result.rows;
+    if(!rows.length) { throw new Error(`Sequence ${name} does not exist`)}
+    return rows[0];
   }
-  async writeSequence(path: string, contents: string): Promise<void> {
-    console.warn(`Write "${contents.slice(0, 50)}..." to ${path} - Not yet implemented`);
+  async writeSequence(name: string, definition: string): Promise<any> {
+    console.warn(`Write "${definition.slice(0, 50)}..." to ${name} - Not yet implemented`);
+    const result = await this.pool.query(`
+      WITH updated AS (
+        UPDATE sequencing.user_sequence
+        SET definition = $3
+        WHERE name = $1 AND workspace_id = $2
+        RETURNING *
+      )
+      INSERT INTO sequencing.user_sequence (name, workspace_id, definition, parcel_id)
+      SELECT $1, $2, $3, $4
+      WHERE NOT EXISTS (SELECT 1 FROM updated);
+    `, [name, this.workspaceId, definition, 1]);
+
+    return result;
   }
 }
 
